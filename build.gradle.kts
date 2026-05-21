7b990b3591fa57c7a0bba3ada453ba5ad82bd75c
Garkatron <masitacarg@gmail.com>
2026-05-21 20:04:32 +0100

    include pathetic in bundle and bumped lib version

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,10 @@ dependencies {
 	implementation(libs.modMenu)
 	implementation(libs.legacyLwjgl)
 	implementation(libs.patheticApi)
+	include(libs.patheticApi)
 	implementation(libs.patheticEngine)
+	include(libs.patheticEngine)
+
 	implementation(libs.slf4jApi)
 	implementation(libs.guava)
 	implementation(libs.log4j.slf4j2.impl)
@@ -66,6 +69,7 @@ dependencies {
 
 	implementation(libs.commonsLang3)
 	include(libs.commonsLang3)
+
 }
 java {
 	toolchain {
